1d10a879a8149d0b9b3e5e328006a6a0a38a2b39
marble <kangsw1025@naver.com>
2021-09-14 20:07:26 +0900

    Add Social Login, Email Login function

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,6 +1,49 @@
-import React from "react";
-function Auth() {
-  return <div>로그인</div>;
+import React, { useState } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+function AuthForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = e => {
+    const { name, value } = e.target;
+
+    if (name === "email") setEmail(value);
+    if (name === "password") setPassword(value);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      let data;
+      data = await signInWithEmailAndPassword(getAuth(), email, password);
+    } catch (e) {
+      setError(e);
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        name="email"
+        type="email"
+        placeholder="Email"
+        onChange={onChange}
+        required
+      />
+      <input
+        name="password"
+        type="password"
+        placeholder="Password"
+        onChange={onChange}
+        required
+      />
+      <input type="submit" value="Login" />
+    </form>
+  );
 }
 
-export default Auth;
+export default AuthForm;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,10 +1,36 @@
 import React from "react";
 import AuthForm from "../components/AuthForm";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
 
 function Auth() {
+  const onSocialClick = async e => {
+    const { name } = e.target;
+    var provider;
+
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    await signInWithPopup(getAuth(), provider);
+  };
+
   return (
     <>
       <AuthForm />
+      <div>
+        <button onClick={onSocialClick} name="google">
+          Login With Google
+        </button>
+        <button onClick={onSocialClick} name="github">
+          Login With Github
+        </button>
+      </div>
     </>
   );
 }
