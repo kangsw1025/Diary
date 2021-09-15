0cb7b28a4d32371f90195a73c82b33f59f42dcbc
marble <kangsw1025@naver.com>
2021-09-15 22:21:21 +0900

    ADD calendar function

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,27 @@
-import React from "react";
-import Calendar from "../components/Calendar";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import TodoList from "../components/TodoList";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/esm/locale";
 
-function Home() {
+function Home({ userObj }) {
+  const [startDate, setStartDate] = useState(new Date());
+  const onChangeDate = date => {
+    setStartDate(date);
+  };
+
   return (
     <>
-      <Header />
+      <Header userObj={userObj} />
       <div>
-        <Calendar />
-        <TodoList />
+        <DatePicker // 달력
+          selected={startDate}
+          locale={ko}
+          dateFormat="yyyy년 MM월 dd일"
+          onChange={onChangeDate}
+        />
+        <TodoList date={startDate} />
       </div>
     </>
   );
